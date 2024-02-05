74361b5b5d371f2f7ab0386cb56b81749d4d5799
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-05 15:02:19 -0300

    first commit

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,39 @@
+package com.jpeccia.gestao_vagas.modules.candidate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity(name = "candidate")
+public class CandidateEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+
+    @Pattern(regexp = "^\\S+", message = "O campo [username] não deve conter espaço.")
+    private String username;
+
+    @Email(message = "O campo (email) deve conter um email válido.")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres.")
+    private String password;
+    private String description;
+    private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
