104b2fcb635149b9b81e64cddc857547e854a522
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-05 15:09:56 -0300

    feat: add candidate repository

--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/CandidateControler.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/CandidateControler.java
@@ -3,9 +3,11 @@ package com.jpeccia.gestao_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpeccia.gestao_vagas.modules.candidate.CandidateEntity;
+import com.jpeccia.gestao_vagas.modules.candidate.CandidateRepository;
 
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class CandidateControler {
     
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create( @Valid @RequestBody CandidateEntity candidateEntity){{
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public @Valid CandidateEntity create( @Valid @RequestBody CandidateEntity candidateEntity){{
+        return this.candidateRepository.save(candidateEntity);
     }}
 
 }
