25b517902baf16c1ca9c3d50284bffdb1ca5343e
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 15:37:18 -0300

    feat: create auth company dto

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/dto/AuthCompanyDTO.java
@@ -0,0 +1,12 @@
+package com.jpeccia.gestao_vagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+  
+    private String username;
+    private String password;
+}
