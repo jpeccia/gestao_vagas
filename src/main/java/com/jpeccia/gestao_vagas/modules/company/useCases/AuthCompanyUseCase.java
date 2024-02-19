ccc5c6f6932eed5ea829bc83b8b18ad966f00e5f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 15:36:10 -0300

    feat: create auth company use case

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,8 @@
+package com.jpeccia.gestao_vagas.modules.company.useCases;
+
+public class AuthCompanyUseCase {
+    
+    public void execute(){
+        
+    }
+}
--- src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,7 +1,6 @@
 package com.jpeccia.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
