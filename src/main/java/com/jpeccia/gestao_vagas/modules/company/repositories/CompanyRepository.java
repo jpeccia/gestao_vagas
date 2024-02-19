50d43324413eb2e92ba4315fb81e8f0584719f62
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 15:42:36 -0300

    feat: update authcompany usecase

--- src/main/java/com/jpeccia/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -6,7 +6,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jpeccia.gestao_vagas.modules.company.entities.CompanyEntity;
+import java.util.List;
+
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity,UUID> {
     Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CompanyEntity> findByUsername(String username);
 }
--- src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,8 +1,32 @@
 package com.jpeccia.gestao_vagas.modules.company.useCases;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.jpeccia.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.jpeccia.gestao_vagas.modules.company.repositories.CompanyRepository;
+
 public class AuthCompanyUseCase {
     
-    public void execute(){
-        
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+            () -> {
+                throw new UsernameNotFoundException("Company not found");
+            });
+
+            var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+            if(!passwordMatches){
+                throw new AuthenticationException();
+            }
     }
 }
