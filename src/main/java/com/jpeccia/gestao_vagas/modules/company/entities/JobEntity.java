3ea485a5ed7f09f5aefc281a12f7cd09726f6b2e
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 15:10:08 -0300

    feat: add nullable in company column

--- src/main/java/com/jpeccia/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/entities/JobEntity.java
@@ -32,7 +32,7 @@ public class JobEntity {
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
-    @Column(name = "company_id")
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
 
     @CreationTimestamp
