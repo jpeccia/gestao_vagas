d91b0f4ad134d1fd706f47ab21e0fdb26f3e70c2
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 14:55:33 -0300

    feat: create job reposity and usecase

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,18 @@
+package com.jpeccia.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpeccia.gestao_vagas.modules.company.entities.JobEntity;
+import com.jpeccia.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+    
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity){
+        return this.jobRepository.save(jobEntity);
+    }
+}
