35c75262d42f9aaac067416a07aa21c377c45d0f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 14:44:37 -0300

    feat: create company controller

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,32 @@
+package com.jpeccia.gestao_vagas.modules.company.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpeccia.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.jpeccia.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/company")
+
+public class CompanyController {
+    
+    @Autowired
+    private CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+        try{
+            var result = this.createCompanyUseCase.execute(companyEntity);
+            return ResponseEntity.ok().body(result);
+        }catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
