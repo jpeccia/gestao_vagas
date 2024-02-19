1434e11b2ca09a2dfe5603883e2cc8d1a4a0214a
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 15:28:20 -0300

    feat: config routes in security config

--- src/main/java/com/jpeccia/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/jpeccia/gestao_vagas/security/SecurityConfig.java
@@ -10,7 +10,14 @@ public class SecurityConfig {
     
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/candidate/").permitAll()
+            .requestMatchers("/company/").permitAll();
+            auth.anyRequest().authenticated();
+        })
+        
+        ;
         return http.build();
     }
 }
