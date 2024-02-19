8b16f24e0c00c14505d39968a01ed871ced6ed96
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-19 15:35:15 -0300

    feat: add password crypt in company use case

--- src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,8 @@
 package com.jpeccia.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jpeccia.gestao_vagas.exceptions.UserFoundException;
@@ -13,6 +15,9 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
          
         this.companyRepository
@@ -20,6 +25,9 @@ public class CreateCompanyUseCase {
         .ifPresent((user) -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
