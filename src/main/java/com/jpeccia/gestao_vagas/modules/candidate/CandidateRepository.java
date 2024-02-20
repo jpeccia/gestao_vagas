12ab3dcae6970f9df24d21113dd244c9c49a865b
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 15:40:56 -0300

    feat: add findByUsername in CandidateRepository

--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
-    
+    Optional<CandidateEntity> findByUsername(String username);
+
 }
