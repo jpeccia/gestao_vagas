9fe5448f0b219c38243bc8af3a3c21bbb9d9f029
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 15:49:16 -0300

    feat: add create candidate token in authcandidate usecasee

--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -1,18 +1,54 @@
 package com.jpeccia.gestao_vagas.modules.candidate.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.jpeccia.gestao_vagas.modules.candidate.CandidateRepository;
 import com.jpeccia.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
 
 @Service
 public class AuthCandidateUseCase {
     
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
     @Autowired
     private CandidateRepository candidateRepository;
 
-    public void execute(AuthCandidateRequestDTO authCandidateRequestDTO) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+        .orElseThrow(() -> {
+            throw new UsernameNotFoundException("Username/password incorrect");
+        });
+
+        var passwordMatches = this.passwordEncoder
+        .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if(!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create()
+            .withIssuer("jpeccia")
+            .withSubject(candidate.getId().toString())
+            .withClaim("roles", Arrays.asList("candidate"))
+            .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+            .sign(algorithm);
 
     }
 }
