dd9686d53417423a3a8da89079aedbb89013afb0
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 15:40:19 -0300

    feat: create authcandidate request DTO

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,5 @@
+package com.jpeccia.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+    
+}
--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -1,11 +1,18 @@
 package com.jpeccia.gestao_vagas.modules.candidate.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jpeccia.gestao_vagas.modules.candidate.CandidateRepository;
+import com.jpeccia.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
 
 @Service
 public class AuthCandidateUseCase {
     
-    public void execute() {
-        
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public void execute(AuthCandidateRequestDTO authCandidateRequestDTO) {
+
     }
 }
