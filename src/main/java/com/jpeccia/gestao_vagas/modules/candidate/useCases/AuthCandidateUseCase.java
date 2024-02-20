18ce413efdad122e06b222946979a7d0cdbdfb88
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 15:37:57 -0300

    feat: create authcandidate useCase

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,11 @@
+package com.jpeccia.gestao_vagas.modules.candidate.useCases;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCandidateUseCase {
+    
+    public void execute() {
+        
+    }
+}
