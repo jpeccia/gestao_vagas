f578d0e6974a5fcf751cec8467c3a718e5654adc
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 16:00:50 -0300

    feat: create authcandidateController

---
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -0,0 +1,38 @@
+package com.jpeccia.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpeccia.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.jpeccia.gestao_vagas.modules.candidate.useCases.AuthCandidateUseCase;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/candidate")
+public class AuthCandidateController {
+
+    @Autowired
+    private AuthCandidateUseCase authCandidateUseCase;
+
+    @PostMapping("/auth")
+    public ResponseEntity<Object> auth(@RequestBody AuthCandidateRequestDTO authCandidateRequestDTO) {
+
+        try {
+            
+            var token = this.authCandidateUseCase.execute(authCandidateRequestDTO);
+            return ResponseEntity.ok().body(token);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+
+    }
+    
+    
+}
