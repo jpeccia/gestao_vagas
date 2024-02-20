11a0e6d200b3421c67e43b18b081afbb0557e724
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 19:49:40 -0300

    feat: add ProfileCandidateUseCase in CandidateController

--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jpeccia.gestao_vagas.modules.candidate.CandidateEntity;
 import com.jpeccia.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import com.jpeccia.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,14 +25,31 @@ public class CandidateController {
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
+    @Autowired
+
+    private ProfileCandidateUseCase profileCandidateUseCase;
+
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){{
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-    }}
+    }
 
-}
+    @GetMapping("/")
+    public ResponseEntity<Object> get() {
+        try {
+            var profile = this.profileCandidateUseCase.execute(null);
+
+            return ResponseEntity.ok().body(profile);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    }
+
+
--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.jpeccia.gestao_vagas.modules.candidate.CandidateEntity;
 import com.jpeccia.gestao_vagas.modules.candidate.CandidateRepository;
 import com.jpeccia.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 
