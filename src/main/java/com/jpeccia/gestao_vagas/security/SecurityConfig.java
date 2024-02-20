8038e9ebbd12647bfff94fa14ab2c4d10766ecd3
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-20 20:11:42 -0300

    feat: add securityCandidateFilter in SecurityConfig

--- src/main/java/com/jpeccia/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/jpeccia/gestao_vagas/security/SecurityConfig.java
@@ -15,6 +15,9 @@ public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
 
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -25,8 +28,10 @@ public class SecurityConfig {
                 .requestMatchers("/candidate/auth").permitAll();
             auth.anyRequest().authenticated();
         })
-        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
-        
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class);
+
+
         ;
         return http.build();
     }
