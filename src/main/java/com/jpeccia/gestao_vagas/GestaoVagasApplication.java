025ba085a6cebd7f1741e80879eafe145d883bdc
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-21 22:19:44 -0300

    feat: insert authentication in swagger

--- src/main/java/com/jpeccia/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/jpeccia/gestao_vagas/GestaoVagasApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -14,6 +17,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 		version = "1"
 	)
 )
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
