9e9b653d250581a69c92e9c286e05c7fb3a80032
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-21 23:25:39 -0300

    feat: add Company Schemes and Examples to API Documentation

--- src/main/java/com/jpeccia/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -2,9 +2,16 @@ package com.jpeccia.gestao_vagas.modules.company.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jpeccia.gestao_vagas.modules.candidate.CandidateEntity;
 import com.jpeccia.gestao_vagas.modules.company.entities.CompanyEntity;
 import com.jpeccia.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +23,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/company")
-
+@Tag(name = "Empresa", description = "Informações da empresa")
 public class CompanyController {
     
     @Autowired
     private CreateCompanyUseCase createCompanyUseCase;
 
     @PostMapping("/")
+    @Operation(summary = "Cadastro da empresa", description = "Essa função é responsável por cadastrar uma empresa.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                schema = @Schema(implementation = CompanyEntity.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Company already exists")
+        })
     public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity) {
         try{
             var result = this.createCompanyUseCase.execute(companyEntity);
--- src/main/java/com/jpeccia/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,15 +27,24 @@ public class CompanyEntity {
     
     @NotBlank()
     @Pattern(regexp = "^\\S+", message = "O campo [username] não deve conter espaço.")
+    @Schema(example = "empresa", requiredMode = RequiredMode.REQUIRED, description = "Usuário da empresa")
     private String username;
 
     @Email(message = "O campo (email) deve conter um email válido.")
+    @Schema(example = "teste@empresa.com", requiredMode = RequiredMode.REQUIRED, description = "E-mail da empresa")
     private String email;
 
     @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres.")
+    @Schema(example = "company@1234", minLength = 10, maxLength = 100, requiredMode = RequiredMode.REQUIRED, description = "Senha da empresa")
     private String password;
+
+    @Schema(example = "www.empresateste.com", requiredMode = RequiredMode.REQUIRED, description = "Site da empresa")
     private String website;
+
+    @Schema(example = "Empresa de desenvolvimento Java", requiredMode = RequiredMode.REQUIRED, description = "Nome da empresa")
     private String name;
+
+    @Schema(example = "Empresa que atua na area de Desenvolvimento Java", requiredMode = RequiredMode.REQUIRED, description = "Breve descrição da empresa")
     private String description;
 
     @CreationTimestamp
