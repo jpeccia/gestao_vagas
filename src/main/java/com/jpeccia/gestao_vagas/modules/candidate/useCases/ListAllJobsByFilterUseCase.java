c997fe175b4afabb1e20c8691ca02d9bb4c20d26
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-21 21:43:07 -0300

    feat: add method findByDescriptionContaing in list all jobsfilter usecase

--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -1,7 +1,10 @@
 package com.jpeccia.gestao_vagas.modules.candidate.useCases;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.jpeccia.gestao_vagas.modules.company.entities.JobEntity;
 import com.jpeccia.gestao_vagas.modules.company.repositories.JobRepository;
 
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -12,8 +15,7 @@ public class ListAllJobsByFilterUseCase {
     @Autowired
     private JobRepository jobRepository;
 
-    public void execute(String filter) {
-
-
+    public List<JobEntity> execute(String filter) {
+        return this.jobRepository.findByDescriptionContaing(filter);
     }
 }
