6f6d34c835be4c2fcc41abdd0b5a1aab3e0ae55f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-21 21:42:18 -0300

    feat: add list findbyDescription containg in JobRepository

--- src/main/java/com/jpeccia/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -1,5 +1,6 @@
 package com.jpeccia.gestao_vagas.modules.company.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import com.jpeccia.gestao_vagas.modules.company.entities.JobEntity;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
     
+    List<JobEntity> findByDescriptionContaing(String filter);
 }
