c5c0d292deb7111e695df577d3426664a162f0f8
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-02-21 22:11:06 -0300

    feat: add Swagger in CandidateController

--- src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/jpeccia/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -8,6 +8,13 @@ import com.jpeccia.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUs
 import com.jpeccia.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import com.jpeccia.gestao_vagas.modules.company.entities.JobEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -66,6 +73,15 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponivel para o candidato", description = "Essa função é responsável por listar todas as vagas disponiveis, baseada no filtro.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))
+            )
+        }
+         )})
     public List<JobEntity> findJobByFilter(@RequestParam String filter) {
         return this.listAllJobsByFilterUseCase.execute(filter);
     }
